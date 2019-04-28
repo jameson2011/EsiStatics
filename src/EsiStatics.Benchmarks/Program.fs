c6ca4f6c1878a8536b4817c4cd50a72beeee5880
Jameson2011 <j7db@hotmail.co.uk>
2019-04-29 00:20:04 +0100

    first benchmark tests

---
+++ src/EsiStatics.Benchmarks/Program.fs
@@ -0,0 +1,17 @@
+﻿namespace EsiStatics.Benchmarks
+
+module Program=
+    open System
+    open BenchmarkDotNet.Running
+
+    [<EntryPoint>]
+    let main argv =
+    
+        let switch =
+            BenchmarkSwitcher [|    typedefof<SolarSystemGetBenchmark>;
+                                    typedefof<SolarSystemScanBenchmark>;
+                                    typedefof<SolarSystemDeepGetBenchmark>
+                                    |]
+    
+        switch.Run argv |> ignore
+        0 
