0835e2b37d585e1245762a946f5535b09ee5ade9
Jameson2011 <j7db@hotmail.co.uk>
2019-04-29 23:11:45 +0100

    mutable priority queue

---
+++ src/EsiStatics/Collections.fs
@@ -0,0 +1,35 @@
+﻿namespace EsiStatics
+
+open System.Collections.Generic
+
+type internal MutablePriorityQueue<'k,'v>()=
+
+    let dictionary = new SortedDictionary<'k,  Queue<'v>>()
+    
+    let getCreateQueue(key: 'k) =
+        let exists,q = dictionary.TryGetValue(key)
+        if not exists then 
+            let q = new Queue<'v>()
+            dictionary.Add(key, q)
+            q
+        else q
+                    
+    let push key value =
+        let q = getCreateQueue key
+        q.Enqueue value
+        
+    let pop() =
+        match dictionary.Keys |> Seq.tryHead with
+        | Some k ->     let q = dictionary.[k]
+                        let result = q.Dequeue()
+
+                        if q.Count = 0 then
+                            dictionary.Remove(k) |> ignore
+                        
+                        Some result
+        | None ->       None
+        
+
+    member this.Push(key, value) = push key value
+
+    member this.Pop() = pop()
