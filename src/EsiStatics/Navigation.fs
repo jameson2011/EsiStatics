65df87c093828b49f1d0b06cb324fa416333c9cf
Jameson2011 <j7db@hotmail.co.uk>
2019-04-30 20:51:48 +0100

    reduce getSystem calls in route finding

--- src/EsiStatics/Navigation.fs
+++ src/EsiStatics/Navigation.fs
@@ -38,53 +38,54 @@ module Navigation=
         |> (*) (euclidean start.Position finish.Position |> float)
     
     let findRoute (distanceOf: SolarSystem -> SolarSystem -> float)  (start: SolarSystem, finish: SolarSystem) =
-        let startId = start.Id;
+        
         let closed = new HashSet<int>()
-        let todo = new MutablePriorityQueue<float, int>()
-        let todo = todo.Push(0., start.Id)
+        let todo = new MutablePriorityQueue<float, SolarSystem>()
+        let todo = todo.Push(0., start)
 
         let cameFrom = new Dictionary<int, int>()
         let gScore = new Dictionary<int, float>()
-        gScore.[startId] <- 0.0
+        gScore.[start.Id] <- 0.0
 
         
 
         let rec backWalk current result  =
             match current with
-            | x when x = startId -> result
+            | x when x = start.Id -> result
             | _ ->  let next = cameFrom.[current]
                     backWalk next (next :: result)
         
         
         
-        let rec find (todo: MutablePriorityQueue<float, int>)= 
+        let rec find (todo: MutablePriorityQueue<float, SolarSystem>)= 
             
             match todo.Pop() with
             | None -> []
             | Some (distance, current) -> 
 
-                        if (current = finish.Id) then                    
-                            backWalk current [ current ]                    
+                        if (current.Id = finish.Id) then                    
+                            backWalk current.Id [ current.Id ]                    
                         else                    
-                            let currentSystem = getSys current
-                            closed.Add current |> ignore
+                            
+                            closed.Add current.Id |> ignore
 
-                            let neighbours = SolarSystems.neighbourIds current
+                            let neighbours = SolarSystems.neighbourIds current.Id
                                                 |> Seq.filter (closed.Contains >> not)                                        
-                                                |> Seq.map (fun s ->  (distanceOf (getSys s ) finish),  s) 
+                                                |> Seq.map (fun s ->    let sys = getSys s
+                                                                        (distanceOf sys finish), sys) 
                                                 |> List.ofSeq
 
                             // adjust best known paths
                             neighbours 
-                                |> Seq.iter (fun (d,s) ->   let tentative = distance + (distanceOf currentSystem (getSys s) )
-                                                            if (gScore.ContainsKey(s) |> not) 
-                                                                    || (gScore.[s] > tentative) then
-                                                                cameFrom.[s] <- current
-                                                                gScore.[s] <- tentative  )
+                                |> Seq.iter (fun (d,s) ->   let tentative = distance + (distanceOf current s)
+                                                            if (gScore.ContainsKey(s.Id) |> not) 
+                                                                    || (gScore.[s.Id] > tentative) then
+                                                                cameFrom.[s.Id] <- current.Id
+                                                                gScore.[s.Id] <- tentative  )
                             // construct todo  
                             neighbours 
-                                    |> Seq.map (fun (d,n) -> gScore.[n] + d, n)   
-                                    |> Seq.iter (fun (p,v) -> todo.Push(p,v) |> ignore)
+                                    |> Seq.map (fun (d,s) -> gScore.[s.Id] + d, s)   
+                                    |> Seq.iter (fun (d,s) -> todo.Push(d,s) |> ignore)
                         
 
                             find todo
