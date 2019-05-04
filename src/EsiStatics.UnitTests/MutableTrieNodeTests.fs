7e2656bc71215c0623b51b1feeb6eee6730da8b9
Jameson2011 <j7db@hotmail.co.uk>
2019-05-04 13:29:06 +0100

    add mutable trie

---
+++ src/EsiStatics.UnitTests/MutableTrieNodeTests.fs
@@ -0,0 +1,55 @@
+﻿namespace EsiStatics.UnitTests
+
+open System
+open System.Linq
+open Xunit
+open FluentAssertions
+open EsiStatics
+
+module MutableTrieNodeTests=
+
+    let words = [| "cba"; "aaa"; "aab"; "aac"; "aba"; "abb"; "abc"; "baa"; "bab" |]
+    let values() = words |> Array.map (fun s -> (s, s))
+
+    
+    [<Fact>]
+    let ``Empty Trie returns empty``() =
+        let trie = MutableTrie.Create([])
+        
+        let matches = trie.Find ""
+
+        matches.Should().BeEmpty("")
+    
+
+    [<Fact>]
+    let ``Search for all - all returned``() =
+        let trie = values() |> MutableTrie.Create
+        
+        let matches = trie.Find ""
+
+        matches.Should().BeEquivalentTo(words)
+    
+    
+    [<Fact>]
+    let ``Search for unkonwn returns empty``() =
+        let trie = values() |> MutableTrie.Create
+        
+        let matches = trie.Find "!"
+
+        matches.Should().BeEmpty("")
+    
+    [<Theory>]
+    [<InlineData("a")>]
+    [<InlineData("aa")>]
+    [<InlineData("aaa")>]
+    [<InlineData("c")>]
+    [<InlineData("!£$!\"")>]
+    let ``Search returns prefixed words``(word) =
+        let trie = values() |> MutableTrie.Create
+        
+        let matches = trie.Find word
+
+        let expected = words |> Seq.filter (fun w -> w.StartsWith(word)) |> List.ofSeq
+
+        matches.SequenceEqual(expected).Should().BeTrue("")
+    
---
+++ src/EsiStatics.UnitTests/Tests.fsx
@@ -0,0 +1,22 @@
+﻿
+#r @"bin\Debug\netcoreapp2.2\EsiStatics.dll"
+#r @"bin\Debug\netcoreapp2.2\EsiStatics.Data.Entities.dll"
+#r @"bin\Debug\netcoreapp2.2\EsiStatics.Data.Universe.dll"
+
+#time
+
+open EsiStatics
+open EsiStatics.Data.Entities
+open EsiStatics.Data.Universe
+
+let solarSystems = Regions.all() 
+                    |> Seq.collect (fun r -> r.Constellations())
+                    |> Seq.collect (fun c -> c.SolarSystems())
+                    |> Array.ofSeq
+
+let indexValues = solarSystems 
+                    |> Array.map (fun s -> (s.Name, (s.Name, s.Id)))
+
+let index =  new EsiStatics.MutableTrie<string * int>(indexValues)
+
+index.Find "adi"
