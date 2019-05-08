0dd47c7995adc1a04f5b8adcae81c040f1c6559b
Jameson2011 <j7db@hotmail.co.uk>
2019-05-09 00:31:48 +0100

    routes are pairwise

--- src/EsiStatics/Navigation.fs
+++ src/EsiStatics/Navigation.fs
@@ -4,23 +4,34 @@ module Navigation=
 
     open System.Collections.Generic
     
-    let euclideanSystemDistance (start: SolarSystem) (finish: SolarSystem) = 
+    let euclideanSystemDistance (start: SolarSystem, finish: SolarSystem) = 
         Geometry.euclidean start.Position finish.Position |> float
         
-    
-    let euclideanSystemDistancePreferHighsec (start: SolarSystem) (finish: SolarSystem) = 
+    let neutral (start: SolarSystem, finish: SolarSystem) = 1.
+
+    let preferHighsec (start: SolarSystem, finish: SolarSystem) = 
+        match start.Level, finish.Level with
+            | SecurityLevel.Highsec, SecurityLevel.Highsec -> 1.
+            | _ -> 100.
+
+    let avoidHighsec (start: SolarSystem, finish: SolarSystem) = 
+        match start.Level, finish.Level with
+            | SecurityLevel.Highsec, SecurityLevel.Highsec -> 100.
+            | _ -> 1.
+
+    let euclideanSystemDistancePreferHighsec (start: SolarSystem, finish: SolarSystem) = 
         match start.Level, finish.Level with
             | SecurityLevel.Highsec, SecurityLevel.Highsec -> 1.
             | _ -> 100.
         |> (*) (Geometry.euclidean start.Position finish.Position |> float)
         
-    let euclideanSystemDistanceAvoidHighsec (start: SolarSystem) (finish: SolarSystem) = 
+    let euclideanSystemDistanceAvoidHighsec (start: SolarSystem, finish: SolarSystem) = 
         match start.Level, finish.Level with
             | SecurityLevel.Highsec, SecurityLevel.Highsec -> 100.
             | _ -> 1.
         |> (*) (Geometry.euclidean start.Position finish.Position |> float)
     
-    let findRoute (distanceOf: SolarSystem -> SolarSystem -> float)  (start: SolarSystem, finish: SolarSystem) =
+    let findRoute (distanceOf: SolarSystem * SolarSystem -> float)  (start: SolarSystem, finish: SolarSystem) =
         
         let closed = new HashSet<int>()
         let todo = new MutablePriorityQueue<float, SolarSystem>()
@@ -53,12 +64,12 @@ module Navigation=
                                                     |> SolarSystems.neighbourIds 
                                                     |> Seq.filter (closed.Contains >> not)                                        
                                                     |> Seq.map (fun id ->   let sys = SolarSystems.knownSystem id
-                                                                            (distanceOf sys finish), sys) 
+                                                                            (distanceOf (sys, finish) ), sys) 
                                                     |> List.ofSeq
 
                                 // adjust best known paths
                                 neighbours 
-                                    |> Seq.iter (fun (_,s) -> let t = distance + (distanceOf current s)
+                                    |> Seq.iter (fun (_,s) -> let t = distance + (distanceOf (current, s))
                                                               if (scores.ContainsKey(s.Id) |> not) || 
                                                                  (scores.[s.Id] > t) then
                                                                 cameFrom.[s.Id] <- current.Id
