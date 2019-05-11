d48d69756d102a50190b89f39e1b99ec0f7697c2
Jameson2011 <j7db@hotmail.co.uk>
2019-05-11 23:31:12 +0100

    add names for C# initialisation

--- src/EsiStatics/Library.fs
+++ src/EsiStatics/Library.fs
@@ -21,12 +21,13 @@ module Library=
         methods 
             |> Array.Parallel.iter (fun mi -> System.Runtime.CompilerServices.RuntimeHelpers.PrepareMethod(mi.MethodHandle) )
         
-
+    [<CompiledName("InitialiseUniverse")>]
     let initUniverse () =
         jitAssembly("EsiStatics.Data.Entities")
         jitAssembly("EsiStatics.Data.Moons")
         jitAssembly("EsiStatics.Data.Universe")
 
+    [<CompiledName("InitialiseItemTypes")>]
     let initItemTypes () =
         jitAssembly("EsiStatics.Data.ItemTypes")
         
