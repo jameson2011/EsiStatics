8ba6374ad7c0d573f9bcaf1c794cb21091667f18
jameson2011 <34738955+jameson2011@users.noreply.github.com>
2020-12-08 01:25:40 +0000

    Feature/jumpdriveroutes (#19)
    * first cut of system distance indexer
    
    * update tests
    
    * update unit tests to core 3.1
    
    * .
    
    * Revert "update tests"
    
    This reverts commit a75e5c50628da17045ecda58d8e458a272c9bf31.
    
    * Revert "update unit tests to core 3.1"
    
    This reverts commit d8ca11721c8d7b634bce4aece4371bc518894dc9.
    
    * Revert "."
    
    This reverts commit a1df1898e59bd3489982f5bcf15b4158858fcfd4.
    
    * :rage:
    
    * first cut
    
    * tweak max distance
    
    * Cheat a little...
    
    * Tweak a little more.
    
    * Sometimes lazy evaluations really are faster...

---
+++ src/EsiStatics.UnitTests/SolarSystemDistanceFinderTests.fs
@@ -0,0 +1,43 @@
+﻿namespace EsiStatics.UnitTests
+
+open System
+open Xunit
+open EsiStatics
+open EsiStatics.Units
+open FluentAssertions
+
+module SolarSystemDistanceFinder=
+        
+    let finder = new SolarSystemFinder()
+    let eagerDistances = new SolarSystemDistanceFinder(true)
+    let lazyDistances = new SolarSystemDistanceFinder(false)
+
+    [<Theory>]
+    [<InlineData("adirain", 2.<LY>, false, 9)>]
+    [<InlineData("adirain", 2.<LY>, true, 2)>]
+    [<InlineData("adirain", 8.<LY>, false, 391)>]
+    [<InlineData("adirain", 8.<LY>, true, 143)>]
+    let ``find neighbours eagerly``(search: string, distance: float<LY>, ignoreHighsec: bool, expectedCount: int)=
+                
+        let system = search  |> finder.Find |> Seq.head
+
+        let neighbours = eagerDistances.Find system distance |> Array.ofSeq
+
+        let neighbours = if ignoreHighsec then neighbours |> Array.filter (fun (s,_) -> s.Level <> SecurityLevel.Highsec) else neighbours                    
+        
+        neighbours.Should().HaveCount(expectedCount,"")
+
+    [<Theory>]
+    [<InlineData("adirain", 2.<LY>, false, 9)>]
+    [<InlineData("adirain", 2.<LY>, true, 2)>]
+    [<InlineData("adirain", 8.<LY>, false, 391)>]
+    [<InlineData("adirain", 8.<LY>, true, 143)>]
+    let ``find neighbours lazily``(search: string, distance: float<LY>, ignoreHighsec: bool, expectedCount: int)=
+                
+        let system = search  |> finder.Find |> Seq.head
+
+        let neighbours = eagerDistances.Find system distance |> Array.ofSeq
+
+        let neighbours = if ignoreHighsec then neighbours |> Array.filter (fun (s,_) -> s.Level <> SecurityLevel.Highsec) else neighbours                    
+        
+        neighbours.Should().HaveCount(expectedCount,"")
--- src/EsiStatics/UniverseSearch.fs
+++ src/EsiStatics/UniverseSearch.fs
@@ -2,6 +2,7 @@
 
 open EsiStatics.Data.Universe
 
+
 type SolarSystemFinder(eagerIndex) =
     
     let index = 
@@ -14,8 +15,7 @@ type SolarSystemFinder(eagerIndex) =
                             |> Seq.map (fun s -> (s.name,  s.id))
                             |> ReadonlyTrie.Create
         )
-
-    
+            
     do  if eagerIndex then index.Value |> ignore
 
     new() = SolarSystemFinder(false)
@@ -26,7 +26,53 @@ type SolarSystemFinder(eagerIndex) =
             |> index.Value.Find 
             |> Seq.map (EsiStatics.SolarSystems.knownSystem)
         
+type SolarSystemDistanceFinder(eagerIndex) =
 
+    let neighbourMaxDistance = 10.1<LY> 
+    
+    let distance (x: Data.Entities.SolarSystemData) (y: Data.Entities.SolarSystemData)= Geometry.euclideanData x.position y.position |> metresToLY
+    
+    let systems = Regions.regions()
+                        |> Seq.collect (fun r -> r.constellationIds)
+                        |> Seq.map (Constellations.getConstellation >> Option.get)
+                        |> Seq.collect (fun c -> c.solarSystemIds)
+                        |> Seq.map (SolarSystems.getSolarSystem >> Option.get)
+                        |> Seq.filter (fun ss ->    ss.secRating = Data.Entities.SystemSecurity.Lowsec || 
+                                                    ss.secRating = Data.Entities.SystemSecurity.Highsec ||
+                                                    ss.secRating = Data.Entities.SystemSecurity.Nullsec)
+    
+    // Approximately O(n^2) 
+    let neighbours (allSystems: Data.Entities.SolarSystemData seq) (system: Data.Entities.SolarSystemData)=                
+        allSystems 
+            |> Seq.filter (fun s -> s <> system)
+            |> Seq.map (fun s -> (distance s system, s.id))
+            |> Seq.filter (fun (d,_) -> d <= neighbourMaxDistance)
+            |> Seq.sortBy fst                    
+            |> Array.ofSeq
+
+    let index =
+        lazy (                                        
+            let systems = systems |> Array.ofSeq
+            systems
+                |> Array.Parallel.map (fun s -> (s.id, neighbours systems s) )
+                |> Map.ofArray
+        )
+            
+    do  if eagerIndex then index.Value |> ignore
+    
+    let findNeighbours (system: SolarSystem) = 
+        match eagerIndex with
+        | true ->   index.Value |> Map.tryFind system.Id
+        | _ ->      system.Id |> SolarSystems.getSolarSystem |> Option.get |> neighbours systems  |> Some
+
+    new() = SolarSystemDistanceFinder(false)
+
+    member this.Find(system: SolarSystem) (distance: float<LY>)= 
+        match findNeighbours system with
+        | Some xs -> xs |> Seq.takeWhile (fun (d, _) -> d <= distance)
+                        |> Seq.map (fun (d, s) -> (SolarSystems.knownSystem s, d)) 
+                        |> Array.ofSeq
+        | _ -> [| |]
 
 type ConstellationFinder(eagerIndex) =
     
