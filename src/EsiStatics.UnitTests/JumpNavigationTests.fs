5f5c2cca9d4c1ac7974db831f3d2e468dfdd4439
Jameson2011 <j7db@hotmail.co.uk>
2021-02-13 15:30:21 +0000

    distance score tests

--- src/EsiStatics.UnitTests/JumpNavigationTests.fs
+++ src/EsiStatics.UnitTests/JumpNavigationTests.fs
@@ -84,7 +84,7 @@ module JumpNavigation=
     [<InlineData(1, 1, 1., 1.)>]
     [<InlineData(1, 2, 1., 0.5)>]    
     [<InlineData(2, 2, 1., 1.)>]    
-    let ``scoreJumpState jumps ``(jumps, maxJumps, weight, expectedScore: float)=
+    let ``scoreJumpState jumps``(jumps, maxJumps, weight, expectedScore: float)=
         let plan = JumpPlan.empty 
                         |> JumpRouteNavigation.emptyStationsWeight 0.
                         |> JumpRouteNavigation.stationDockingWeight 0.
@@ -96,6 +96,31 @@ module JumpNavigation=
         let system = { defaultSystem with id = 1 }
         let stats = { defaultStats with maxJumps = maxJumps }
         let stage = { defaultStage with jumps = Some jumps} 
+
+        let score =  stage |> JumpNavigation.scoreJumpState stats plan system
+
+        score.Should().Be(expectedScore, "")
+
+    [<Theory>]
+    [<InlineData(0, 0, 0., 0.)>]    
+    [<InlineData(1000, 2000, 0., 0.)>]
+    [<InlineData(0, 0, 1000., 0.)>]
+    [<InlineData(0, 1000, 1., 0.)>]
+    [<InlineData(1000, 1000, 1., 1.)>]
+    [<InlineData(1000, 2000, 1., 0.5)>]    
+    [<InlineData(2000, 2000, 1., 1.)>]    
+    let ``scoreJumpState distances``(distance, maxDistance, weight, expectedScore: float)=
+        let plan = JumpPlan.empty 
+                        |> JumpRouteNavigation.emptyStationsWeight 0.
+                        |> JumpRouteNavigation.stationDockingWeight 0.
+                        |> JumpRouteNavigation.distanceWeight weight
+                        |> JumpRouteNavigation.avoidPochvenWeight 0.
+                        |> JumpRouteNavigation.killsWeight 0.
+                        |> JumpRouteNavigation.jumpsWeight 0.
+
+        let system = { defaultSystem with id = 1 }
+        let stats = { defaultStats with maxDistance = maxDistance }
+        let stage = { defaultStage with distanceToDestination = distance} 
 
         let score =  stage |> JumpNavigation.scoreJumpState stats plan system
 
